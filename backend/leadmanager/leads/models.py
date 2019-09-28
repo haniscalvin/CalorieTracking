50c07e3212fd568f431ee8368bf0d1fe0de118fe
Calvin <calvinhan13@gmail.com>
2019-09-27 19:16:04 -0700

    arrange files

---
+++ backend/leadmanager/leads/models.py
@@ -0,0 +1,9 @@
+# from django.db import models
+
+# class Food(models.Model);
+#     food_name = models.CharField(max_length=50)
+#     calories = (max_length=100)
+#     total_fat = (=50)
+#     total_fat = (maximum=50)
+#     total_fat = (maximum=50)
+#     total_fat = (max_length=50)
