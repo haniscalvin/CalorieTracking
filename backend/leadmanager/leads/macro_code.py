8bc524e54ddfabb4a8b80a886a85d5670bf88c42
4542elgh <4542elgh@gmail.com>
2019-09-28 19:00:14 -0700

    add macro code in json format

---
+++ backend/leadmanager/leads/macro_code.py
@@ -0,0 +1,590 @@
+macro_code = {
+	"301": {
+		"macro": "Calcium, Ca", 
+		"unit": "mg"
+	},
+	"205": {
+		"macro": "Carbohydrate,", 
+		"unit": "g"
+	},
+	"601": {
+		"macro": "Cholesterol", 
+		"unit": "m"
+	},
+	"208": {
+		"macro": "Energy", 
+		"unit": "kcal"
+	},
+	"606": {
+		"macro": "Fatty acids, total saturated", 
+		"unit": "g"
+	},
+	"204": {
+		"macro": "Total lipid (fat)", 
+		"unit": "g"
+	},
+	"605": {
+		"macro": "Fatty acids, total trans", 
+		"unit": "g"
+	},
+	"303": {
+		"macro": "Iron, Fe", 
+		"unit": "mg"
+	},
+	"291": {
+		"macro": "Fiber, total dietary", 
+		"unit": "g"
+	},
+	"306": {
+		"macro": "Potassium, K", 
+		"unit": "mg"
+	},
+	"307": {
+		"macro": "Sodium, Na", 
+		"unit": "mg"
+	},
+	"203": {
+		"macro": "Protein", 
+		"unit": "g"
+	},
+	"269": {
+		"macro": "Sugars, total", 
+		"unit": "g"
+	},
+	"539": {
+		"macro": "Sugars, added", 
+		"unit": "g"
+	},
+	"324": {
+		"macro": "Vitamin D", 
+		"unit": "IU"
+	},
+	"513": {
+		"macro": "Alanine", 
+		"unit": "g"
+	},
+	"221": {
+		"macro": "Alcohol, ethyl", 
+		"unit": "g"
+	},
+	"511": {
+		"macro": "Arginine", 
+		"unit": "g"
+	},
+	"207": {
+		"macro": "Ash", 
+		"unit": "g"
+	},
+	"514": {
+		"macro": "Aspartic acid", 
+		"unit": "g"
+	},
+	"454": {
+		"macro": "Betaine", 
+		"unit": "mg"
+	},
+	"262": {
+		"macro": "Caffeine", 
+		"unit": "mg"
+	},
+	"639": {
+		"macro": "Campesterol", 
+		"unit": "mg"
+	},
+	"322": {
+		"macro": "Carotene, alpha", 
+		"unit": "Âµg"
+	},
+	"321": {
+		"macro": "Carotene, beta", 
+		"unit": "Âµg"
+	},
+	"326": {
+		"macro": "Vitamin D3 (cholecalciferol)", 
+		"unit": "Âµg"
+	},
+	"421": {
+		"macro": "Choline, total", 
+		"unit": "mg"
+	},
+	"334": {
+		"macro": "Cryptoxanthin, beta", 
+		"unit": "Âµg"
+	},
+	"312": {
+		"macro": "Copper, Cu", 
+		"unit": "mg"
+	},
+	"507": {
+		"macro": "Cystine", 
+		"unit": "g"
+	},
+	"268": {
+		"macro": "Energy", 
+		"unit": "kJ"
+	},
+	"325": {
+		"macro": "Vitamin D2 (ergocalciferol)", 
+		"unit": "Âµg"
+	},
+	"610": {
+		"macro": "10:00", 
+		"unit": "g"
+	},
+	"611": {
+		"macro": "12:00", 
+		"unit": "g"
+	},
+	"696": {
+		"macro": "13:00", 
+		"unit": "g"
+	},
+	"612": {
+		"macro": "14:00", 
+		"unit": "g"
+	},
+	"625": {
+		"macro": "14:01", 
+		"unit": "g"
+	},
+	"652": {
+		"macro": "15:00", 
+		"unit": "g"
+	},
+	"697": {
+		"macro": "15:01", 
+		"unit": "g"
+	},
+	"613": {
+		"macro": "16:00", 
+		"unit": "g"
+	},
+	"626": {
+		"macro": "16:1 undifferentiated", 
+		"unit": "g"
+	},
+	"673": {
+		"macro": "16:1 c", 
+		"unit": "g"
+	},
+	"662": {
+		"macro": "16:1 t", 
+		"unit": "g"
+	},
+	"653": {
+		"macro": "17:00", 
+		"unit": "g"
+	},
+	"687": {
+		"macro": "17:01", 
+		"unit": "g"
+	},
+	"614": {
+		"macro": "18:00", 
+		"unit": "g"
+	},
+	"617": {
+		"macro": "18:1 undifferentiated", 
+		"unit": "g"
+	},
+	"674": {
+		"macro": "18:1 c", 
+		"unit": "g"
+	},
+	"663": {
+		"macro": "18:1 t", 
+		"unit": "g"
+	},
+	"859": {
+		"macro": "18:1-11t (18:1t n-7)", 
+		"unit": "g"
+	},
+	"618": {
+		"macro": "18:2 undifferentiated", 
+		"unit": "g"
+	},
+	"670": {
+		"macro": "18:2 CLAs", 
+		"unit": "g"
+	},
+	"675": {
+		"macro": "18:2 n-6 c,c", 
+		"unit": "g"
+	},
+	"669": {
+		"macro": "18:2 t,t", 
+		"unit": "g"
+	},
+	"619": {
+		"macro": "18:3 undifferentiated", 
+		"unit": "g"
+	},
+	"851": {
+		"macro": "18:3 n-3 c,c,c (ALA)",
+		"unit": "g"
+	},
+	"685": {
+		"macro": "18:3 n-6 c,c,c", 
+		"unit": "g"
+	},
+	"627": {
+		"macro": "18:04", 
+		"unit": "g"
+	},
+	"615": {
+		"macro": "20:00", 
+		"unit": "g"
+	},
+	"628": {
+		"macro": "20:01", 
+		"unit": "g"
+	},
+	"672": {
+		"macro": "20:2 n-6 c,c", 
+		"unit": "g"
+	},
+	"689": {
+		"macro": "20:3 undifferentiated", 
+		"unit": "g"
+	},
+	"852": {
+		"macro": "20:3 n-3", 
+		"unit": "g"
+	},
+	"853": {
+		"macro": "20:3 n-6", 
+		"unit": "g"
+	},
+	"620": {
+		"macro": "20:4 undifferentiated", 
+		"unit": "g"
+	},
+	"855": {
+		"macro": "20:4 n-6", 
+		"unit": "g"
+	},
+	"629": {
+		"macro": "20:5 n-3 (EPA)",
+		"unit":"g"
+	},
+	"857": {
+		"macro": "21:05", 
+		"unit": "g"
+	},
+	"624": {
+		"macro": "22:00", 
+		"unit": "g"
+	},
+	"630": {
+		"macro": "22:1 undifferentiated", 
+		"unit": "g"
+	},
+	"858": {
+		"macro": "22:04", 
+		"unit": "g"
+	},
+	"631": {
+		"macro": "22:5 n-3 (DPA)", 
+		"unit": "g"
+	},
+	"621": {
+		"macro": "22:6 n-3 (DHA)",
+		"unit": "g"
+	},
+	"654": {
+		"macro": "24:00:00", 
+		"unit": "g"
+	},
+	"671": {
+		"macro": "24:1 c", 
+		"unit": "g"
+	},
+	"607": {
+		"macro": "4:00", 
+		"unit": "g"
+	},
+	"608": {
+		"macro": "6:00", 
+		"unit": "g"
+	},
+	"609": {
+		"macro": "8:00", 
+		"unit": "g"
+	},
+	"645": {
+		"macro": "Fatty acids, total monounsaturated", 
+		"unit": "g"
+	},
+	"646": {
+		"macro": "Fatty acids, total polyunsaturated", 
+		"unit": "g"
+	},
+	"693": {
+		"macro": "Fatty acids, total trans-monoenoic", 
+		"unit": "g"
+	},
+	"695": {
+		"macro": "Fatty acids, total trans-polyenoic", 
+		"unit": "g"
+	},
+	"313": {
+		"macro": "Fluoride, F", 
+		"unit": "Âµg"
+	},
+	"417": {
+		"macro": "Folate, total", 
+		"unit": "Âµg"
+	},
+	"431": {
+		"macro": "Folic acid", 
+		"unit": "Âµg"
+	},
+	"435": {
+		"macro": "Folate, DFE", 
+		"unit": "Âµg"
+	},
+	"432": {
+		"macro": "Folate, food", 
+		"unit": "Âµg"
+	},
+	"212": {
+		"macro": "Fructose", 
+		"unit": "g"
+	},
+	"287": {
+		"macro": "Galactose", 
+		"unit": "g"
+	},
+	"515": {
+		"macro": "Glutamic acid", 
+		"unit": "g"
+	},
+	"211": {
+		"macro": "Glucose (dextrose)", 
+		"unit": "g"
+	},
+	"516": {
+		"macro": "Glycine", 
+		"unit": "g"
+	},
+	"512": {
+		"macro": "Histidine", 
+		"unit": "g"
+	},
+	"521": {
+		"macro": "Hydroxyproline", 
+		"unit": "g"
+	},
+	"503": {
+		"macro": "Isoleucine", 
+		"unit": "g"
+	},
+	"213": {
+		"macro": "Lactose", 
+		"unit": "g"
+	},
+	"504": {
+		"macro": "Leucine", 
+		"unit": "g"
+	},
+	"338": {
+		"macro": "ZEA Lutein + zeaxanthin", 
+		"unit": "Âµg"
+	},
+	"337": {
+		"macro": "Lycopene", 
+		"unit": "Âµg"
+	},
+	"505": {
+		"macro": "Lysine", 
+		"unit": "g"
+	},
+	"214": {
+		"macro": "Maltose", 
+		"unit": "g"
+	},
+	"506": {
+		"macro": "Methionine", 
+		"unit": "g"
+	},
+	"304": {
+		"macro": "Magnesium, Mg", 
+		"unit": "mg"
+	},
+	"428": {
+		"macro": "Menaquinone-4", 
+		"unit": "Âµg"
+	},
+	"315": {
+		"macro": "Manganese, Mn", 
+		"unit": "mg"
+	},
+	"406": {
+		"macro": "Niacin", 
+		"unit": "mg"
+	},
+	"573": {
+		"macro": "Vitamin E, added", 
+		"unit": "mg"
+	},
+	"578": {
+		"macro": "Vitamin B-12, added", 
+		"unit": "Âµg"
+	},
+	"257": {
+		"macro": "Adjusted Protein", 
+		"unit": "g"
+	},
+	"664": {
+		"macro": "22:1 t", 
+		"unit": "g"
+	},
+	"676": {
+		"macro": "22:1 c", 
+		"unit": "g"
+	},
+	"856": {
+		"macro": "18:3i", 
+		"unit": "g"
+	},
+	"665": {
+		"macro": "18:2 t not further defined", 
+		"unit": "g"
+	},
+	"666": {
+		"macro": "18:2 i", 
+		"unit": "g"
+	},
+	"305": {
+		"macro": "Phosphorus, P", 
+		"unit": "mg"
+	},
+	"410": {
+		"macro": "Pantothenic acid", 
+		"unit": "mg"
+	},
+	"508": {
+		"macro": "Phenylalanine", 
+		"unit": "g"
+	},
+	"636": {
+		"macro": "Phytosterols", 
+		"unit": "mg"
+	},
+	"517": {
+		"macro": "Proline", 
+		"unit": "g"
+	},
+	"319": {
+		"macro": "Retinol", 
+		"unit": "Âµg"
+	},
+	"405": {
+		"macro": "Riboflavin", 
+		"unit": "mg"
+	},
+	"317": {
+		"macro": "Selenium, Se", 
+		"unit": "Âµg"
+	},
+	"518": {
+		"macro": "Serine", 
+		"unit": "g"
+	},
+	"641": {
+		"macro": "Beta-sitosterol", 
+		"unit": "mg"
+	},
+	"209": {
+		"macro": "Starch", 
+		"unit": "g"
+	},
+	"638": {
+		"macro": "Stigmasterol", 
+		"unit": "mg"
+	},
+	"210": {
+		"macro": "Sucrose", 
+		"unit": "g"
+	},
+	"263": {
+		"macro": "Theobromine", 
+		"unit": "mg"
+	},
+	"404": {
+		"macro": "Thiamin", 
+		"unit": "mg"
+	},
+	"502": {
+		"macro": "Threonine", 
+		"unit": "g"
+	},
+	"323": {
+		"macro": "Vitamin E (alpha-tocopherol)", 
+		"unit": "mg"
+	},
+	"341": {
+		"macro": "Tocopherol, beta", 
+		"unit": "mg"
+	},
+	"343": {
+		"macro": "Tocopherol, delta", 
+		"unit": "mg"
+	},
+	"342": {
+		"macro": "Tocopherol, gamma", 
+		"unit": "mg"
+	},
+	"501": {
+		"macro": "Tryptophan", 
+		"unit": "g"
+	},
+	"509": {
+		"macro": "Tyrosine", 
+		"unit": "g"
+	},
+	"510": {
+		"macro": "Valine", 
+		"unit": "g"
+	},
+	"318": {
+		"macro": "Vitamin A, IU", 
+		"unit": "IU"
+	},
+	"320": {
+		"macro": "Vitamin A, RAE", 
+		"unit": "Âµg"
+	},
+	"418": {
+		"macro": "Vitamin B-12", 
+		"unit": "Âµg"
+	},
+	"415": {
+		"macro": "Vitamin B-6", 
+		"unit": "mg"
+	},
+	"401": {
+		"macro": "Vitamin C, total ascorbic acid", 
+		"unit": "mg"
+	},
+	"328": {
+		"macro": "Vitamin D (D2 + D3)", 
+		"unit": "Âµg"
+	},
+	"430": {
+		"macro": "Vitamin K (phylloquinone)", 
+		"unit": "Âµg"
+	},
+	"429": {
+		"macro": "Dihydrophylloquinone", 
+		"unit": "Âµg"
+	},
+	"255": {
+		"macro": "Water", 
+		"unit": "g"
+	},
+	"309": {
+		"macro": "Zinc, Zn",
+		"unit": "mg"
+	}
+}
--- backend/leadmanager/leads/views.py
+++ backend/leadmanager/leads/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 import requests
 
 # Create your views here.
 
 def home(request):
-    response = requests.get('http://freegeoip.net/json/')
-    geodata = response.json()
-    return render(request, 'test.html', {
-    })
+    response = requests.post('https://trackapi.nutritionix.com/v2/natural/nutrients', headers={"x-app-id":"4e13e5aa","x-app-key":"2371b979c007fdf88758574145c58171"},data={"query":"eggs for breakfast"})
+    return JsonResponse(response.json())
